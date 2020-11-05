61a892424df9bee5e1b2fc54f0ca5395cff91eb1
Niriksha27 <73120854+Niriksha27@users.noreply.github.com>
2020-11-05 17:00:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+		public static void Main()
+		{
+			Console.WriteLine("Enter the first Number");
+			int x = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Type *,/,+,-");
+			string z = Console.ReadLine();
+			Console.WriteLine("Enter the second Number");
+			int y = Convert.ToInt32(Console.ReadLine());
+			if (z == "+")
+			{
+				Console.WriteLine("Answer: {0}", x + y);
+			}
+			if (z == "-")
+			{
+				Console.WriteLine("Answer: {0}", x - y);
+			}
+			if (z == "*")
+			{
+				Console.WriteLine("Answer: {0}", x * y);
+			}
+			if (z == "/")
+			{
+				Console.WriteLine("Answer: {0}", x / y);
+			}
+
+		}
+	}
+}
+
